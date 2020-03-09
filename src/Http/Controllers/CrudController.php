568e9ce610290fc046ca518db83a01f261bd7bca
danieletulone <danieletulone.work@gmail.com>
2020-03-09 09:49:08 +0100

    Updated CrudController

--- src/Http/Controllers/CrudController.php
+++ src/Http/Controllers/CrudController.php
@@ -3,10 +3,10 @@
 
 namespace DanieleTulone\BaseCrud\Controllers;
 
-use DanieleTulone\BaseCrud\ClassHelper;
-use DanieleTulone\BaseCrud\ViewHelper;
-
-use Validator;
+use DanieleTulone\BaseCrud\Traits\HasCrudQueries;
+use DanieleTulone\BaseCrud\Traits\Sideable;
+use DanieleTulone\BaseCrud\Traits\Validable;
+use DanieleTulone\BaseCrud\Helpers\ViewHelper;
 
 /**
  * Base controller used for basic crud.
@@ -17,88 +17,18 @@ use Validator;
  */
 class CrudController extends Controller
 {
-    /**
-     * FormRequest used for validate data.
-     * 
-     * @var mixed
-     */
-    public $formRequest;
-
-    /**
-     * Messages used to give feedback to user.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
-     * @var string[][]
-     */
-    public $messages = [
-        "destroy" => [
-            "success" => "Record rimosso correttamente.",
-            "error" => "Non è stato possibile rimuovere il record."
-        ],
-        "store" => [
-            "success" => "Record creato correttamente.",
-            "error" => "Non è stato possibile creare il record."
-        ],
-        "update" => [
-            "success" => "Record aggiornato correttamente.",
-            "error" => "Non è stato possibile aggiornare il record."
-        ]
-    ];
+    use HasCrudQueries, Sideable ,Validable;
 
     /**
      * Model to use with this controller.
      * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
      * @var mixed
      */
-    public $model;
+    protected $model;
 
     /**
-     * Set if this controller return view() as response or work only as api
-     * 
-     * @var mixed
-     */
-    public $useView; 
-
-    /**
-     * Make operations after action.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
-     * @param mixed $method 
-     * @param mixed $params
-     * @return void 
-     */
-    private function callAfterAction($method, &$params)
-    {
-        if (ClassHelper::hasTrait(get_called_class(), 'DanieleTulone\BaseCrud\Traits\HasAfterActions')) {
-            $method = ucfirst($method);
-            $this->{"after{$method}"}($params);
-        }
-    }
-
-    /**
-     * Make operations before action.
-     * 
-     * @param mixed $method 
-     * @param mixed $params 
-     * @return void 
-     */
-    private function callBeforeAction($method, &$params)
-    {        
-        if (ClassHelper::hasTrait(get_called_class(), 'DanieleTulone\BaseCrud\Traits\HasBeforeActions')) {
-            $method = ucfirst($method);
-            $this->{"before{$method}"}($params);
-        }
-    }
-
-    /**
-     * Get all params from route url and from GET, POST, JSON.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
+     * Get all params from route url and from request.
+     *  
      * @return array 
      */
     public function getParams()
@@ -112,12 +42,10 @@ class CrudController extends Controller
     /**
      * Default destroy action.
      * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
      * @param mixed $id 
      * @return mixed 
      */
-    final public function destroy()
+    public function destroy()
     {
         $params = $this->getParams();
         
@@ -135,11 +63,9 @@ class CrudController extends Controller
     /**
      * Default index action.
      * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
      * @return mixed 
      */
-    final public function index()
+    public function index()
     {
         $params = $this->getParams();
 
@@ -153,22 +79,8 @@ class CrudController extends Controller
     }
 
     /**
-     * Query used for index action.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     * 
-     * @return mixed
-     */
-    public function indexQuery(&$params)
-    {
-        return $this->model::paginate();
-    }
-
-    /**
      * Redirect to a view. 
      * Automatically redirect to admin or web routes.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
      * 
      * @param mixed $flag 
      * @param mixed $method 
@@ -180,11 +92,11 @@ class CrudController extends Controller
     }
 
     /**
-     * 
+     * Show the resource.
      * 
      * @return Illuminate\View\View|Illuminate\Contracts\View\Factory 
      */
-    final public function show()
+    public function show()
     {   
         $params = $this->getParams();
 
@@ -195,11 +107,6 @@ class CrudController extends Controller
         return $this->response($params, __FUNCTION__);
     }
 
-    public function showQuery(&$params)
-    {
-        return $this->model::findOrFail($params['model']);
-    }
-
     /**
      * Default store action.
      * 
@@ -208,17 +115,15 @@ class CrudController extends Controller
      * @param Illuminate\Http\Request $request 
      * @return mixed 
      */
-    final public function store()
+    public function store()
     {
         $params = $this->getParams();
 
         $this->callBeforeAction(__FUNCTION__ , $params);
 
-        $rules = $this->formRequest::getRules($params);
-        
-        $data = Validator::make($params, $rules)->validate();
+        $data = $this->validate($params);
 
-        $params["model"] = $this->model::create($data);
+        $params["model"] = $this->storeQuery($data);
 
         $this->callAfterAction(__FUNCTION__ , $params);
 
@@ -234,17 +139,15 @@ class CrudController extends Controller
      * @param mixed $data 
      * @return mixed 
      */
-    final public function update()
+    public function update()
     {   
         $params = $this->getParams();
 
         $this->callBeforeAction(__FUNCTION__ , $params);
-
-        $rules = $this->formRequest::getRules($params);
         
-        $data = Validator::make($params, $rules)->validate();
+        $data = $this->validate($params);
 
-        $params["updated"] = $this->model::findOrFail($params["model"])->update($data);
+        $params["updated"] = $this->updateQuery($params, $data);
 
         $this->callAfterAction(__FUNCTION__ , $params);
 
