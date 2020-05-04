3848d09625ed3bcd2856aa044a53477f2af6a04e
danieletulone <danieletulone.work@gmail.com>
2020-05-04 17:37:19 +0200

    Add autodiscovery package

---
+++ src/Providers/BaseCrudServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DanieleTulone\BaseCrud\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseCrudServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/basecrud.php' => config_path('basecrud.php'),
+        ], 'config');
+        
+        $this->publishes([
+            __DIR__.'/../Http/Controllers/CrudController.php' => app_path('Http/Controllers/CrudController.php'),
+            __DIR__.'/../Http/Controllers/RestCrudController.php' => app_path('Http/Controllers/RestCrudController.php'),
+        ], 'controllers');
+    }
+}
---
+++ src/config/basecrud.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'modelsPath' => app_path('Models')
+];
