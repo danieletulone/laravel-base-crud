3884a1620eeadc2d4cbaa705d717d349ea0f20dd
danieletulone <danieletulone.work@gmail.com>
2020-05-10 18:33:12 +0200

    Remove pointer to params

--- src/Traits/HasCrudQueries.php
+++ src/Traits/HasCrudQueries.php
@@ -2,14 +2,16 @@
 
 namespace DanieleTulone\BaseCrud\Traits;
 
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 trait HasCrudQueries
 {
     /**
      * Get model name by class namespace.
-     * 
-     * @return string 
+     *
+     * @return string
      */
     final private function getModelName()
     {
@@ -17,87 +19,104 @@ trait HasCrudQueries
     }
 
     /**
-     * Get data or params. When there are validated 
+     * Get data or params. When there are validated
      * data return data, otherwise params.
-     * 
-     * @param mixed $params 
-     * @return mixed 
+     *
+     * @param mixed $params
+     * @return mixed
      */
-    final private function getDataOrParams($params)
+    final private function getDataOrParams()
     {
+        $params = $this->getParams();
+
         if (isset($params["data"])) {
             return $params["data"];
         }
-        
+
         return $params;
     }
 
     /**
      * Query used for delete method.
-     * 
-     * @param mixed $params 
+     *
+     * @param mixed $params
      * @return mixed
      */
-    public function destroyQuery(&$params)
+    public function destroyQuery()
     {
         $modelName = $this->getModelName();
 
-        $params["deleted"] = $this->model::findOrFail($params[$modelName])->delete();
+        $this->params["deleted"] = $this->model::findOrFail($this->params[$modelName])->delete();
     }
 
     /**
      * Query used for index method.
-     * 
-     * @param mixed $params 
+     *
+     * @param mixed $params
      * @return mixed
      */
-    public function indexQuery(&$params)
+    public function indexQuery()
     {
+        if ($this->formRequest != null) {
+            resolve($this->formRequest);
+        }
+
         $modelsName = Str::plural($this->getModelName());
 
-        $params[$modelsName] = $this->model::paginate();
+        $this->params[$modelsName] = $this->model::paginate();
     }
 
     /**
      * Query used for show method
-     * 
-     * @param mixed $params 
-     * @return mixed 
+     *
+     * @param mixed $params
+     * @return mixed
      */
-    public function showQuery(&$params)
+    public function showQuery()
     {
         $modelName = $this->getModelName();
 
-        $params[$modelName] = $this->model::findOrFail($params[$modelName]);
+        $this->params = $this->model::findOrFail($this->params[$modelName]);
     }
 
     /**
      * Query used for store method.
-     * 
-     * @param mixed $data 
-     * @return mixed 
+     *
+     * @param mixed $data
+     * @return mixed
      */
-    public function storeQuery(&$params)
+    public function storeQuery()
     {
+        if ($this->formRequest != null) {
+            resolve($this->formRequest);
+        }
+
+        $params = $this->getParams();
         $data = $this->getDataOrParams($params);
         $modelName =$this->getModelName();
 
-        $params[$modelName] = $this->model::create($data);
+        $this->params[$modelName] = $this->model::create($data);
     }
 
     /**
      * Query used for update method.
-     * 
-     * @param mixed $params 
-     * @param mixed $data 
-     * @return mixed 
+     *
+     * @param mixed $params
+     * @param mixed $data
+     * @return mixed
      */
-    public function updateQuery(&$params)
+    public function updateQuery()
     {
+        if ($this->formRequest != null) {
+            resolve($this->formRequest);
+        }
+
+        $params = $this->getParams();
+
         $modelName = $this->getModelName();
 
         $data = $this->getDataOrParams($params);
 
-        $params["updated"] = $this->model::findOrFail($params[$modelName])->update($data);
+        $this->params["updated"] = $this->model::findOrFail($params[$modelName])->update($data);
     }
 }
--- src/Traits/Sideable.php
+++ src/Traits/Sideable.php
@@ -3,50 +3,52 @@
 namespace DanieleTulone\BaseCrud\Traits;
 
 /**
- * This trait add to methods the capacity to execute 
+ * This trait add to methods the capacity to execute
  * actions before of after primary code of method
  * It is like oppositive concept of middleware
- * 
+ *
  * @package DanieleTulone\BaseCrud\Trais
  */
 trait Sideable
 {
+
     /**
      * Make operations after action.
-     * 
-     * @param mixed $method 
+     *
+     * @param mixed $method
      * @param mixed $params
-     * @return void 
+     * @return void
      */
-    final protected function callAfterAction($method, &$params)
+    final protected function callAfterAction($method)
     {
-        $this->callSideAction("after", $method, $params);
+        $this->callSideAction("after", $method);
     }
 
     /**
      * Make operations before action.
-     * 
-     * @param mixed $method 
-     * @param mixed $params 
+     *
+     * @param mixed $method
+     * @param mixed $params
      * @return void
      */
-    final protected function callBeforeAction($method, &$params)
-    {        
-        $this->callSideAction("before", $method, $params);
+    final protected function callBeforeAction($method)
+    {
+        $this->callSideAction("before", $method);
     }
 
     /**
      * Make operation by side.
-     * 
-     * @param mixed $side 
-     * @param mixed $method 
-     * @param mixed $params 
-     * @return void 
+     *
+     * @param mixed $side
+     * @param mixed $method
+     * @param mixed $params
+     * @return void
      */
-    final private function callSideAction($side, $method, &$params)
+    final private function callSideAction($side, $method)
     {
         $method = ucfirst($method);
         $sideMethod = "{$side}{$method}";
+        $params = $this->getParams();
 
         if (method_exists($this, $sideMethod)) {
             $this->{$sideMethod}($params);
